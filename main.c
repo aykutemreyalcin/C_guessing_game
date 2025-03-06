30f58dc6bd3eac8436b32a442b0b4456fd18ef24
Aykut <76070532+aykutemreyalcin@users.noreply.github.com>
2025-03-06 05:23:44 +0100

    Update main.c
    adding the difficulity option

--- main.c
+++ main.c
@@ -2,30 +2,83 @@
 #include <stdlib.h>
 #include <time.h>
 
-void guessing_game() {
+void guessing_game(difficulity) {
     int number, guess, attempts = 0;
-    srand(time(0));
-    number = rand() % 100 + 1; // Generates a random number between 1 and 100
     
-    printf("Welcome to the Guessing Game!\n");
-    printf("Try to guess the number between 1 and 100.\n");
-    
-    do {
-        printf("Enter your guess: ");
-        scanf("%d", &guess);
-        attempts++;
+    if (difficulity == 3)
+    {
+        srand(time(0));
+        number = rand() % 100 + 1; // Generates a random number between 1 and 100
+        printf("Welcome to the Guessing Game!\n");
+        printf("Try to guess the number between 1 and 100.\n");
+        do {
+            printf("Enter your guess: ");
+            scanf("%d", &guess);
+            attempts++;
         
-        if (guess > number) {
-            printf("Too high! Try again.\n");
-        } else if (guess < number) {
-            printf("Too low! Try again.\n");
-        } else {
-            printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
-        }
-    } while (guess != number);
+            if (guess > number) {
+                printf("Too high! Try again.\n");
+            } else if (guess < number) {
+                printf("Too low! Try again.\n");
+            } else {
+                printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
+            }
+        } while (guess != number);
+    }
+    else if (difficulity == 2)
+    {
+        srand(time(0));
+        number = rand() % 50 + 1;
+        printf("Welcome to the Guessing Game!\n");
+        printf("Try to guess the number between 1 and 50.\n");
+        do {
+            printf("Enter your guess: ");
+            scanf("%d", &guess);
+            attempts++;
+        
+            if (guess > number) {
+                printf("Too high! Try again.\n");
+            } else if (guess < number) {
+                printf("Too low! Try again.\n");
+            } else {
+                printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
+            }
+        } while (guess != number);
+    }
+    else
+    {
+        srand(time(0));
+        number = rand() % 10 + 1;
+        printf("Welcome to the Guessing Game!\n");
+        printf("Try to guess the number between 1 and 10.\n");
+        do {
+            printf("Enter your guess: ");
+            scanf("%d", &guess);
+            attempts++;
+        
+            if (guess > number) {
+                printf("Too high! Try again.\n");
+            } else if (guess < number) {
+                printf("Too low! Try again.\n");
+            } else {
+                printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
+            }
+        } while (guess != number);
+    }
 }
 
 int main() {
-    guessing_game();
-    return 0;
+    while (1)
+        {
+        printf("what difficulity do you want\n1 --> 1-10\n2 --> 1-50\n3 --> 1-100\n");
+        int difficulity;
+        scanf("%d", &difficulity);
+        if (difficulity < 1)
+        {
+            printf("enter a valid option");
+            break;
+        }
+        guessing_game(difficulity);
+        return 0;
+    }
 }
